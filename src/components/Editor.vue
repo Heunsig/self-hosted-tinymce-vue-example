33ae84f9415cf0c3992f66f0d8a14511f79caf60
Heunsig <heun3344@gmail.com>
2024-02-01 22:45:01 +0900

    tinymce-vue 사용 예시1, 2 추가

---
+++ src/components/Editor.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Editor from '@tinymce/tinymce-vue'
+</script>
+
+<template>
+  <main id="sample">
+    <Editor
+      api-key="no-api-key"
+      :init="{
+        plugins: 'lists link image table code help wordcount'
+      }"
+    />
+  </main>
+</template>
