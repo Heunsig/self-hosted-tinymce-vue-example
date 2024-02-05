2ef418718a47a3b774de6809d1538515c62e2c22
Heunsig <heun3344@gmail.com>
2024-02-05 22:55:01 +0900

    set up self hosted tinymce

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
 import Editor from '@/components/Editor.vue'
-// import Editor2 from '@/components/Editor2.vue'
 </script>
 
 <template>
   <main id="sample">
     <Editor/>
-    <!-- <Editor2/> -->
   </main>
 </template>
 
